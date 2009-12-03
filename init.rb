a13902cac59c41e92a9ca7f5d0c3bcfc772e4949
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-12-03 14:11:43 +0600

    Garbage collected

--- init.rb
+++ init.rb
@@ -2,4 +2,3 @@ require 'rubygems'
 require 'active_support'
 __DIR__ = File.dirname(__FILE__)
 Dir.glob(File.join(__DIR__, 'lib', '**', '*.rb')).each { |file| require file }
-#asdfoasindf oasdifna sodf
