dcbec67fcaa527ada7a8d76361500068f27c351a
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-12-03 14:10:03 +0600

    Fixing console application

--- parse!.rb
+++ parse!.rb
@@ -1,13 +1,11 @@
 #!/usr/bin/ruby
 
-def get_results (file_name)
-  __DIR__ = File.dirname(__FILE__)
-  require File.join(__DIR__, 'init')
+__DIR__ = File.dirname(__FILE__)
+require File.join(__DIR__, 'init')
 
-  user_address = '77.235.9.36'
+user_address = '77.235.9.36'
+file_name = 'sample.log'
 
-  traffic_calculator = TrafficCalculator.new file_name, user_address
-  traffic_calculator.calculate!
-  #traffic_calculator.print_results
-  traffic_calculator.get_results
-end
+traffic_calculator = TrafficCalculator.new file_name, user_address
+traffic_calculator.calculate!
+traffic_calculator.print_results
