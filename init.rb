21eccc56af20a29b5a9986889ac1f46f3e1fd8b7
Daniel Vartanov <daniel.vartanov@gmail.com>
2009-12-03 14:09:43 +0600

    Meet new require conventions

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-require 'activesupport'
+require 'active_support'
 __DIR__ = File.dirname(__FILE__)
 Dir.glob(File.join(__DIR__, 'lib', '**', '*.rb')).each { |file| require file }
 #asdfoasindf oasdifna sodf
