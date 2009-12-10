a573a109aa5fd9d3cd01603e9b6da1038e409615
DanielVartanov <daniel.vartanov@gmail.com>
2009-12-10 12:22:59 +0600

    Record slightly refactored

--- lib/record.rb
+++ lib/record.rb
@@ -21,9 +21,13 @@ class Record < Struct.new(:source_address, :destination_address, :bytes,
 protected
 
   def day_bounds
-    day_start = Time.mktime transferred_at.year, transferred_at.month, transferred_at.day, 8, 00
-    day_end = Time.mktime transferred_at.year, transferred_at.month, transferred_at.day, 23, 59
+    day_start = on_the_day_at 8,00
+    day_end = on_the_day_at 23,59
 
     day_start..day_end
+  end
+
+  def on_the_day_at(hours, minutes)
+    Time.mktime transferred_at.year, transferred_at.month, transferred_at.day, hours, minutes
   end
 end
